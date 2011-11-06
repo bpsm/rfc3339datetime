b43a621fd1c3ec4a6acfa1d6eecde101e05ce080
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2011-11-06 18:33:18 +0100

    init

---
+++ src/rfc3339datetime/core.clj
@@ -0,0 +1,1 @@
+(ns rfc3339datetime.core)
---
+++ test/rfc3339datetime/test/core.clj
@@ -0,0 +1,6 @@
+(ns rfc3339datetime.test.core
+  (:use [rfc3339datetime.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
