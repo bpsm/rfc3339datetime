86ea5d044a37969af8849c075ad06aee19c9efbf
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2011-11-07 00:24:01 +0100

    provide (parse s), which parses rfc3339-style timestamp
          - time stamp can elide trailing components
          - time-offset is optional
          - an absent time-offset is taken to be UTC (i.e. treated as if
            +00:00 had been specified.)
    
    Parse is pluggable, the sense that it takes a constructor function which
    is called with the the result of the parse. The value returned by the
    constructor is ultimately the result of the call to parse.
    
    constructors are provided for the Java types java.util.Date,
    java.util.Calendar and java.sql.Timestamp.
    
    Functions are provided to format each of these three types
    as rfc3339 timestamps, with some possible minor lossage
    of information depending on which type is chosen:
    
    - Calendar preserves only milliseconds
    - Date preserves only milliseconds and forgets the timezone
      offset the original literal used.
    - Timestamp preserves nanoseconds (yea), but forgets the
      timezone.
    
    When I say "forgets the timezone" I mean that if we
    format the object in question back to an rfc3339
    timestamp, we'll end up with something like:
    
    2000-01-02T12:00:00+00:00 and it won't be possible
    to tell if the original literal ended with 13:00:00+01:00
    or maybe 06:00:00-06:00. (All three forms specify the
    same instant in time, so there's no real lossage.)

--- src/rfc3339datetime/core.clj
+++ src/rfc3339datetime/core.clj
@@ -1,1 +1,220 @@
-(ns rfc3339datetime.core)
+(ns rfc3339datetime.core
+  (:import [java.util Calendar Date GregorianCalendar TimeZone]
+           [java.sql Timestamp]))
+
+(declare parse-timestamp validated)
+
+;;;
+;;; parse method
+;;;
+
+(defn parse
+  "Parse a string containing an RFC3339-like like timestamp.
+
+The function constructor is called with the following arguments.
+
+                min  max           default
+                ---  ------------  -------
+  years          0          9'999      N/A (s must provide years)
+  months         1             12        1
+  days           1             31        1 (actual max days depends
+  hours          0             23        0  on month and year)
+  minutes        0             59        0
+  seconds        0             60        0 (though 60 is only valid             
+  nanoseconds    0    999'999'999        0  when minutes is 59)
+  offset-sign   -1              1        0
+  offset-hours   0             23        0
+  offset-minutes 0             59        0
+
+These are all integers and will be non-nil. (The listed defaults
+will be passed if the corresponding field is not present in s.)
+
+Grammar (of s):
+
+  date-fullyear   = 4DIGIT
+  date-month      = 2DIGIT  ; 01-12
+  date-mday       = 2DIGIT  ; 01-28, 01-29, 01-30, 01-31 based on
+                            ; month/year
+  time-hour       = 2DIGIT  ; 00-23
+  time-minute     = 2DIGIT  ; 00-59
+  time-second     = 2DIGIT  ; 00-58, 00-59, 00-60 based on leap second
+                            ; rules
+  time-secfrac    = '.' 1*DIGIT
+  time-numoffset  = ('+' / '-') time-hour ':' time-minute
+  time-offset     = 'Z' / time-numoffset
+
+  time-part       = time-hour [ ':' time-minute [ ':' time-second 
+                    [time-secfrac] [time-offset] ] ]
+
+  timestamp       = date-year [ '-' date-month [ '-' date-mday 
+                    [ 'T' time-part ] ] ]
+
+Unlike RFC3339:
+
+  - we only consdier timestamp (was 'date-time')
+    (removed: 'full-time', 'full-date')
+  - timestamp can elide trailing components
+  - time-offset is optional
+
+Though time-offset is syntactically optional, a missing time-offset
+will be treated as if the time-offset zero (+00:00) had been
+specified.
+"
+  [constructor s]
+  (parse-timestamp (validated constructor) s))
+
+;;;
+;;; constructor functions
+;;;
+
+(defn construct-string
+  "Construct an RFC3339 compatible string"
+  [years months days hours minutes seconds nanoseconds
+   offset-sign offset-hours offset-minutes]
+  (format "%04d-%02d-%02dT%02d:%02d:%02d.%09d%s%02d:%02d"
+          years months days hours minutes seconds
+          nanoseconds (if (neg? offset-sign) "-" "+")
+          offset-hours offset-minutes))
+
+(defn construct-calendar
+  "Construct a java.util.Calendar, which preserves, preserving the timezone
+offset, but truncating the subsecond fraction to milliseconds."
+  ^GregorianCalendar
+  [years months days hours minutes seconds nanoseconds
+   offset-sign offset-hours offset-minutes]
+  (doto (GregorianCalendar. years (dec months) days hours minutes seconds)
+    (.set Calendar/MILLISECOND (/ nanoseconds 1000000))
+    (.setTimeZone (TimeZone/getTimeZone
+                   (format "GMT%s%02d:%02d"
+                           (if (neg? offset-sign) "-" "+")
+                           offset-hours offset-minutes)))))
+
+(defn construct-date
+  "Construct a java.util.Date, which expresses the original instant as
+milliseconds since the epoch, GMT."
+  [years months days hours minutes seconds nanoseconds
+   offset-sign offset-hours offset-minutes]
+  (.getTime (construct-calendar years months days
+                                hours minutes seconds nanoseconds
+                                offset-sign offset-hours offset-minutes)))
+
+(defn construct-timestamp
+  "Construct a java.sql.Timestamp, which has nanosecond precision."
+  [years months days hours minutes seconds nanoseconds
+   offset-sign offset-hours offset-minutes]
+  (doto (Timestamp.
+         (.getTimeInMillis
+          (construct-calendar years months days
+                              hours minutes seconds nanoseconds
+                              offset-sign offset-hours offset-minutes)))
+    (.setNanos nanoseconds)))
+
+
+;;;
+;;; convert various parse results back to rfc3339 datetime strings
+;;;
+
+(defn substring
+  ([^String s ^long begin-index ^long end-index]
+     (.substring s begin-index end-index))
+  ([^String s ^long begin-index]
+     (.substring s begin-index)))
+
+(defn fixup-offset [s]
+  (let [x (- (count s) 2)]
+    (str (substring s 0 x) ":" (substring s x))))
+
+(def calendar->rfc3339
+  (comp fixup-offset
+        (partial format "%1$tFT%1$tT.%1$tL%1$tz")))
+
+(def date->rfc3339 calendar->rfc3339)
+
+(def timestamp->rfc3339
+  (comp fixup-offset
+        (partial format "%1$tFT%1$tT.%1$tN%1$tz")))
+
+;;;
+;;; parser implementation
+;;;
+
+(defmacro assoc-if [map test key val]
+  `(let [map# ~map]
+     (if ~test (assoc map# ~key ~val) map#)))
+
+(defn parse-int [^String s]
+  (Long/parseLong s))
+
+(defmacro fail [msg]
+  `(throw (RuntimeException. ~msg)))
+
+(defn zero-fill-right [^String s width]
+  (cond (= width (count s)) s
+        (< width (count s)) (substring s 0 width)
+        :else (loop [b (StringBuilder. s)]
+                (if (< (.length b) width)
+                  (recur (.append b \0))
+                  (.toString b)))))
+
+(def parse-timestamp
+  (let [re #"(\d\d\d\d)(?:-(\d\d)(?:-(\d\d)(?:[T](\d\d)(?::(\d\d)(?::(\d\d)(?:[.](\d+))?)?)?)?)?)?(?:[Z]|([-+])(\d\d):(\d\d))?"]
+    (fn [constructor ^CharSequence cs]
+      (if-let [[_ years months days hours minutes seconds fraction
+                offset-sign offset-hours offset-minutes]
+               (re-matches re cs)]
+        (constructor
+         (parse-int years)
+         (if-not months   1 (parse-int months))
+         (if-not days     1 (parse-int days))
+         (if-not hours    0 (parse-int hours))
+         (if-not minutes  0 (parse-int minutes))
+         (if-not seconds  0 (parse-int seconds))
+         (if-not fraction 0 (parse-int (zero-fill-right fraction 9)))
+         (cond (= "-" offset-sign) -1
+               (= "+" offset-sign)  1
+               :else                0)
+         (if-not offset-hours   0 (parse-int offset-hours))
+         (if-not offset-minutes 0 (parse-int offset-minutes)))
+        (fail (str "Unrecognized date/time syntax: " cs))))))
+
+;;;
+;;; Verification of Extra-Grammatical Restrictions from RFC3339
+;;;
+
+(defmacro divisible? [num div] `(zero? (mod ~num ~div)))
+(defmacro indivisible? [num div] `(not (divisible? ~num ~div)))
+
+(defn leap-year? [year]
+  (and (divisible? year 4)
+       (or (indivisible? year 100)
+           (divisible? year 400))))
+
+(def days-in-month
+  (let [dim [nil 31 28 31 30 31 30 31 31 30 31 30 31]]
+    (fn [month leap-year?]
+      (+ (dim month)
+         (if (and (= month 2) leap-year?) 1 0)))))
+
+(defmacro verify
+  ([test msg] `(when-not ~test (fail ~msg)))
+  ([test] `(verify ~test ~(str "failed: " (pr-str test)))))
+
+(defn validated
+  "Middleware to which validates inputs before passing them on to
+constructor."
+  [constructor]
+  (fn [years months days hours minutes seconds nanoseconds
+       offset-sign offset-hours offset-minutes]
+    (verify (<= 1 months 12))
+    (verify (<= 1 days (days-in-month months (leap-year? years))))
+    (verify (<= 0 hours 23))
+    (verify (<= 0 minutes 59))
+    (verify (<= 0 seconds (if (= minutes 59) 60 59)))
+    (verify (<= 0 nanoseconds 999999999))
+    (verify (<= -1 offset-sign 1))
+    (verify (<= -23 offset-hours 23))
+    (verify (<= -59 offset-minutes 59))
+    (constructor years months days hours minutes seconds nanoseconds
+                 offset-sign offset-hours offset-minutes)))
+
+
--- test/rfc3339datetime/test/core.clj
+++ test/rfc3339datetime/test/core.clj
@@ -2,5 +2,55 @@
   (:use [rfc3339datetime.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest good-cases
+  (testing "eliding trailing fields, omitting time zone offset"
+    (are [p s] (= p (parse construct-string s))
+         "2001-01-01T00:00:00.000000000+00:00" "2001"
+         "2001-02-01T00:00:00.000000000+00:00" "2001-02"
+         "2001-02-03T00:00:00.000000000+00:00" "2001-02-03"
+         "2001-02-03T04:00:00.000000000+00:00" "2001-02-03T04"
+         "2001-02-03T04:05:00.000000000+00:00" "2001-02-03T04:05"
+         "2001-02-03T04:05:06.000000000+00:00" "2001-02-03T04:05:06"
+         "2001-02-03T04:05:06.987654321+00:00" "2001-02-03T04:05:06.987654321"))
+  (testing "various time zone offsets"
+    (are [p s] (= p (parse construct-string s))
+         "2001-01-01T00:00:00.000000000+00:00" "2001Z"
+         "2001-02-01T00:00:00.000000000+07:08" "2001-02+07:08"
+         "2001-02-01T00:00:00.000000000-11:17" "2001-02-11:17")))
+
+(deftest leap-year
+  (testing "feburary 29 is forbidden if not leap year"
+    (is (thrown? RuntimeException (parse construct-string "1900-02-29")))
+    (is (thrown? RuntimeException (parse construct-string "1999-02-29"))))
+  (testing "february 29 is allowed on leap years"
+    (are [p s] (= p (parse construct-string s))
+         "2004-02-29T00:00:00.000000000+00:00" "2004-02-29"
+         "2000-02-29T00:00:00.000000000+00:00" "2000-02-29")))
+
+(deftest leap-seconds
+  (testing "leap second is forbidden if minutes is not 59"
+    (is (thrown? RuntimeException
+                 (parse construct-string "2000-01-02T00:58:60"))))
+  (testing "leap second is allowed if minutes is 59"
+    (is (= "1999-11-30T00:59:60.000000000+00:00"
+           (parse construct-string "1999-11-30T00:59:60")))))
+
+(deftest restrictions
+  (are [s] (thrown? RuntimeException (parse construct-string s))
+       "12000-02-03T04:05:06.987654321-07:08"
+       "2000-13-03T04:05:06.987654321-07:08"
+       "2004-02-30T04:05:06.987654321-07:08"
+       "2004-09-31T04:05:06.987654321-07:08"
+       "2004-12-32T04:05:06.987654321-07:08"
+       "2000-02-03T24:05:06.987654321-07:08"
+       "2000-02-03T04:60:06.987654321-07:08"
+       "2000-02-03T04:05:60.987654321-07:08"
+       "2000-02-03T04:59:61.987654321-07:08"
+       "2000-02-03T04:05:06.987654321-24:08"
+       "2000-02-03T04:05:06.987654321-07:60"
+       "2000-12-03t04:05:06.987654321-07:08"
+       "2000-12-03T04:05:06.987654321z"))
+       
+  
+
+
